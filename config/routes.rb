6a7d1a1b8a2733879547a46183628e7382f322cd
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-26 18:14:30 -0400

    Update the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-resources :users
-resources :recipes
+  devise_for :users
+  
+  resources :users, only: [] do
+    get 'recipes_list', on: :collection
+    delete 'delete_recipe/:id', to: 'users#delete_recipe', on: :member, as: :delete_recipe
+  end
+
+  resources :recipes, only: [] do
+    get 'public_recipes_list', on: :collection
+    get 'recipe_details/:id', to: 'recipes#recipe_details', on: :member, as: :recipe_details
+    post 'toggle_public/:id', to: 'recipes#toggle_public', on: :member, as: :toggle_public
+  end
+
+  root 'recipes#public_recipes_list'
 end
