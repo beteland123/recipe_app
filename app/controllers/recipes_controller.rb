09f499e778d0aa4e939004c1e7ae5e8fa7998e9d
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-25 22:06:14 -0400

    fix rubocop issue

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,34 +1,33 @@
 class RecipesController < ApplicationController
-	  def index
-	@recipes = Recipe.all
+  def index
+    @recipes = Recipe.all
   end
 
   def show
-	@recipe = Recipe.find_by(id: params[:id])
+    @recipe = Recipe.find_by(id: params[:id])
   end
 
-  def new
-  end
+  def new; end
 
   def create
-	@recipe = Recipe.new
-	@recipe.name = params[:name]
-	@recipe.ingredients = params[:ingredients]
-	@recipe.cook_time = params[:cook_time]
-	@recipe.save
-	redirect_to recipe_path(@recipe)
+    @recipe = Recipe.new
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect_to recipe_path(@recipe)
   end
 
   def edit
-	@recipe = Recipe.find_by(id: params[:id])
+    @recipe = Recipe.find_by(id: params[:id])
   end
 
   def update
-	@recipe = Recipe.find_by(id: params[:id])
-	@recipe.name = params[:name]
-	@recipe.ingredients = params[:ingredients]
-	@recipe.cook_time = params[:cook_time]
-	@recipe.save
-	redirect_to recipe_path(@recipe)
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect_to recipe_path(@recipe)
   end
 end
