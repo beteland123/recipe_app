2d2c2778c65ff2de80e000c626da2c23bf2dd6cb
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-25 23:10:13 -0400

    Create recipe list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 resources :users
-resources :recipes
+resources :recipes do
+    member do
+      post 'toggle_public'
+    end
+  end
+  get 'public_recipes', to: 'recipes#public_index', as: :public_recipes
 end
