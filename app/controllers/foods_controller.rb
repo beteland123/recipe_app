d08f6c318409fee58a37c7d9dea25833fec9f96a
beteland123 <beteland23@gmail.com>
2023-09-26 17:24:14 +0300

    fix rubcop

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,35 +1,35 @@
 class FoodsController < ApplicationController
-      def index
-        @foods = current_user.foods
-      end
-    
-      def new
-        @current_user = current_user
-        @food = Food.new
-      end
-    
-      def create
-        @food = current_user.foods.build(food_params)
-    
-        if @food.save
-          flash[:success] = 'Food saved successfully'
-          redirect_to foods_url
-        else
-            flash.now[:error] = 'Error: Food could not be saved'
-            render 'new'
-        end
-      end
-    
-      def destroy
-        @food = Food.find(params[:id])
-        @food.destroy
-    
-        redirect_to foods_url
-      end
-    
-      private
-       
-      def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-      end
+  def index
+    @foods = current_user.foods
+  end
+
+  def new
+    @current_user = current_user
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.foods.build(food_params)
+
+    if @food.save
+      flash[:success] = 'Food saved successfully'
+      redirect_to foods_url
+    else
+      flash.now[:error] = 'Error: Food could not be saved'
+      render 'new'
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+
+    redirect_to foods_url
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
