c00c1a204586774b4b006014824706c260ac4437
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-28 22:50:39 -0400

    fix rubocop issues

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,16 +7,11 @@ class RecipesController < ApplicationController
     @public_recipes = Recipe.where(public: true).order(created_at: :desc)
   end
 
-  def public_recipes
-    @public_recipes = Recipe.where(public: true).order(created_at: :desc)
-    render 'public_index'
-  end
-
   def show
-	@recipe = Recipe.find(params[:id])
-	@recipe_foods = @recipe.recipe_foods
-	@user = current_user
-	@food = Food.new if current_user == @recipe.user
+    @recipe = Recipe.find(params[:id])
+    @recipe_foods = @recipe.recipe_foods
+    @user = current_user
+    @food = Food.new if current_user == @recipe.user
   end
 
   def new
@@ -48,22 +43,22 @@ class RecipesController < ApplicationController
   end
 
   def toggle_public
-	@recipe = Recipe.find(params[:id])
-	
-	if current_user == @recipe.user
-	  @recipe.toggle!(:public)
-	  
-	  if @recipe.public?
-		notice_message = "Recipe is now public."
-	  else
-		notice_message = "Recipe is now private."
-	  end
-	else
-	  notice_message = "You are not authorized to update this recipe."
-	end
-	
-	redirect_to recipe_path(@recipe), notice: notice_message
-  end  
+    @recipe = Recipe.find(params[:id])
+
+    if current_user == @recipe.user
+      @recipe.toggle!(:public)
+
+      notice_message = if @recipe.public?
+                         'Recipe is now public.'
+                       else
+                         'Recipe is now private.'
+                       end
+    else
+      notice_message = 'You are not authorized to update this recipe.'
+    end
+
+    redirect_to recipe_path(@recipe), notice: notice_message
+  end
 
   private
 
@@ -85,8 +80,8 @@ class RecipesController < ApplicationController
   end
 
   def add_ingredient
-	@recipe = Recipe.find(params[:id])
-	@food = Food.new
+    @recipe = Recipe.find(params[:id])
+    @food = Food.new
   end
 
   def public_recipes
